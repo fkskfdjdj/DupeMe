64603d8fa770939f188680601c3a1f8d131c19a6
Nightly <pixel@nightly.eu.org>
2025-04-30 17:33:36 +0300

    1.2.0: 1.21.5 support, `/dupe N` support

--- src/main/kotlin/org/nightlyeu/DupeMe.kt
+++ src/main/kotlin/org/nightlyeu/DupeMe.kt
@@ -1,8 +1,11 @@
 package org.nightlyeu
 
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
 import org.slf4j.LoggerFactory
 
 object DupeMe : ModInitializer {
@@ -12,16 +15,29 @@ object DupeMe : ModInitializer {
 		CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _, _ ->
 			dispatcher.register(
 				CommandManager.literal("dupe")
-					.executes { context ->
-						val source = context.source
-						val player = source.player ?: return@executes 0
-						val item = player.mainHandStack
-						player.giveItemStack(item.copy())
-						1
-					}
-			)
+					.executes { dupe(it) }
+					.then(CommandManager.argument("count", IntegerArgumentType.integer(1))
+						.executes { dupe(it) }
+			))
 		})
 
 		logger.info("DupeMe: Server ready!")
+	}
+
+	fun dupe(context: CommandContext<ServerCommandSource>): Int {
+		val source = context.source
+		val player = source.player ?: return 0
+		val item = player.mainHandStack
+		val amt =
+		try {
+			IntegerArgumentType.getInteger(context, "count")
+		} catch (_: IllegalArgumentException) {
+			return 1
+		}
+
+		for (i in 1..amt) {
+			player.giveItemStack(item.copy())
+		}
+		return 1
 	}
 }
